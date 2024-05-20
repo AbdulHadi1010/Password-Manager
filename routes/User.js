3a1d75784494792347cbc152b2f3536bc486563a
Abdul Hadi <hadi2408466@gmail.com>
2024-05-20 12:02:21 +0500

    Update User.js

--- routes/User.js
+++ routes/User.js
@@ -5,16 +5,19 @@ const router = express.Router();
 import { UserModel } from "../models/User.js";
 
 router.post("/signup", async (req, res) => {
-    const { name, id, password } = req.body;
-    const user = await UserModel.findOne({ id });
-    if (user) {
-      return res.status(400).json({ message: "id already exists" });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new UserModel({ name, id, password: hashedPassword });
-    await newUser.save();
-    res.json({ message: "User registered successfully" });
-  });
+  res.json({ message: "Hello world" });
+});
+router.post("/signup", async (req, res) => {
+  const { name, id, password } = req.body;
+  const user = await UserModel.findOne({ id });
+  if (user) {
+    return res.status(400).json({ message: "id already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new UserModel({ name, id, password: hashedPassword });
+  await newUser.save();
+  res.json({ message: "User registered successfully" });
+});
 
 router.post("/signin", async (req, res) => {
   const { id, password } = req.body;
