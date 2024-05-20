8ba57053e6e7d9e522845092090a9eda8ccf5e00
root <root@ip-172-31-1-174.ap-south-1.compute.internal>
2024-05-20 07:38:32 +0000

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-router.post("/", async (req, res) => {
+app.use("/", async (req, res) => {
   res.json({ message: "Hello world" });
 });
 app.use("/User", userRouter);
